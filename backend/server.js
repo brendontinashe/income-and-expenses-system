8efdf3b6fecb2bb7ae23f9e4b96a867a8a57f073
brendontinashe <Brendonbuwe@gmail.com>
2025-04-27 23:02:45 +0200

    backend

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+// Load env vars
+dotenv.config();
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/incomes', require('./routes/incomeRoutes'));
+app.use('/api/expenses', require('./routes/expenseRoutes'));
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+}); 
